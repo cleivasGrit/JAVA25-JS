fb64f2310685841c8724fbea43e2c0ca95f6a73f
cleivas <clara.leivas@gritacademy.se>
2026-03-27 15:40:50 +0100

    added a higher order callback example

--- 03 - events 1/js/higherorder.js
+++ 03 - events 1/js/higherorder.js
@@ -35,3 +35,26 @@ higherOrder( function(){console.log('anonym')} )
 const double = x => x+x;
 
 higherOrder( () => console.log('arrow') )
+
+
+// Ett lite mer praktiskt exempel
+// Den här funktionen har i uppgift att loopa igenom en array med nummer, utföra en uträkning på varje nummer och returnera resultaten i en ny array.
+function map(numberArr, calcFunction){
+    const results = [];
+    let i = 0;
+
+    for(const num of numberArr){
+        results[i] = calcFunction(num);
+        i++;
+    }
+    return results;
+};
+
+const arr = [3, 14, 55, 8, 4, 34, 82, 3];
+
+// Genom att calcFunction går att byta ut går det att använda map på flera olika sätt
+console.log( map(arr, x=>x*x));
+console.log( map(arr, x=>x*2));
+console.log( map(arr, x=>x/100));
+console.log( map(arr, x=>x*x*x));
+
